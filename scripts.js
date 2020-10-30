0fc26c860140213c0922558192c948c7ce3f49af
Jamar Lee <jamar.lee30@gmail.com>
2020-10-30 13:20:43 -0500

    Added a few touchups to style.css

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
 /* Add your JavaScript to this file */
-"use strict"
+
 "use strict"
 document.addEventListener("DOMContentLoaded", function(){
+	
     const submit = document.getElementsByTagName('button')[0];
-    const email = document.getElementById('email');
-    const message = document.getElementsByClassName('message')[0];
+	const message = document.getElementsByClassName('message')[0];
+    var email = document.getElementById('email');
+	
     submit.addEventListener('click', function(event){
         console.log(email.value);
         event.preventDefault();
-        if (email.value == ''){
-            message.innerHTML = 'Please enter a valid email address'
+		
+        if(email.value == ""){
+            message.innerHTML = "Please enter a valid email address."
         }
-        else { 
+        else{ 
             message.innerHTML = "Thank you! Your email address, ${email.value}, has been added to our mailing list!";
         }
     });
+	
 });
