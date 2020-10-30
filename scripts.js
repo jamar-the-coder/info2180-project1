8ceef781fb239fd5aaa351669c9cec4b0c05613c
Jamar Lee <jamar.lee30@gmail.com>
2020-10-30 12:35:09 -0500

    Added functionality to scripts.js.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+"use strict"
+document.addEventListener("EventListenerForJavaScript", function(){
+	
+	const email = document.getElementById('email');
+    const submit = document.getElementsByTagName("button")[0];
+    const message = document.getElementsByClassName("message")[0];
+	
+    submit.addEventListener('onclick', function(){
+		
+        if(email.value == ""){
+            message.innerHTML = "Please enter a valid email address."
+        }
+        else{ 
+            message.innerHTML = "Thank you! Your email address", ${email.value}, "has been added to our mailing list!";
+        }
+		
+    });
+});
