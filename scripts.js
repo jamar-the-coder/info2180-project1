2d219e8fe13a2d2f1d79aa7b86b5c9ad391cfb14
Jamar Lee <jamar.lee30@gmail.com>
2020-11-01 18:45:36 -0500

    Added a few stylistics changes to styles.css

--- scripts.js
+++ scripts.js
@@ -8,14 +8,13 @@ document.addEventListener("DOMContentLoaded", function(){
     var email = document.getElementById('email');
 	
     submit.addEventListener('click', function(event){
-        console.log(email.value);
         event.preventDefault();
 		
         if(email.value == ""){
             message.innerHTML = "Please enter a valid email address."
         }
         else{ 
-            message.innerHTML = "Thank you! Your email address, ${email.value}, has been added to our mailing list!";
+            message.innerHTML = "Thank you! Your email address has been added to our mailing list!";
         }
     });
 	
